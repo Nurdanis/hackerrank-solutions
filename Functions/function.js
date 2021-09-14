75156a2fbd3bedecb2ab193f679264c8c36b224f
nur <nurdanturan97@gmail.com>
2021-09-14 11:19:17 +0300

    add comment

--- Functions/function.js
+++ Functions/function.js
@@ -1,3 +1,19 @@
+/* 
+
+Objective
+
+Today, we're discussing JavaScript functions. Check out the attached tutorial for more details.
+
+Task
+
+Implement a function named factorial that has one parameter: an integer,n . It must return the value of  (i.e., n factorial).
+
+Input Format
+
+Locked stub code in the editor reads a single integer, n, from stdin and passes it to a function named factorial.
+
+
+*/
 
 function factorial(n){
     if(n === 0){
